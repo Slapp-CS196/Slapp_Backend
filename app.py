59ac4785083631e04f5b6348efedf16509eb5a9b
Rohan Mathur <rohanmathur34@gmail.com>
2015-09-23 21:14:39 -0500

    More api work
    Signed-off-by: Rohan Mathur <rohanmathur34@gmail.com>

--- app.py
+++ app.py
@@ -1,9 +1,34 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+events = [
+    {
+        "date_detected": 1929,
+        "latitude": 40,
+        "longitude": 43
+    },
+    {
+        "date_detected": 4934,
+        "latitude": 41.56,
+        "longitude": 46.89
+    },
+    {
+        "date_detected": 6436,
+        "latitude": 47.56,
+        "longitude": 46.39
+    }
+]
+
+
+@app.route('/api/events', methods=['GET'])
+def return_all():
+    return jsonify({"events" : events})
+
+@app.route('/api/nearby', methods=['GET'])
+def return_nearby():
+    returned_events = [event for event in events if event['latitude'] < 41]
+    return jsonify({"events" : returned_events})
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)

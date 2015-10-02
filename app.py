4e51f2f243dcddc50f8e523e3dec2b4c65f66386
Rohan Mathur <rohanmathur34@gmail.com>
2015-10-01 20:06:58 -0500

    Add more database stuff
    Signed-off-by: Rohan Mathur <rohanmathur34@gmail.com>

--- app.py
+++ app.py
@@ -1,42 +1,37 @@
 from flask import Flask, jsonify, request
-from models import Base, Slapp
+from models import db, Slapp
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
-events = [
-    {
-        "date_detected": 1929,
-        "latitude": 40,
-        "longitude": 43
-    },
-    {
-        "date_detected": 4934,
-        "latitude": 41.56,
-        "longitude": 46.89
-    },
-    {
-        "date_detected": 6436,
-        "latitude": 47.56,
-        "longitude": 46.39
-    }
-]
+@app.route('/api/new', methods=['GET'])
+def create_new():
+    if 'user_id' and 'time' and 'latitude' and 'longitude' in request.args:
+        user_id = request.args['user_id']
+        time = request.args['time']
+        latitude = request.args['latitude']
+        longitude = request.args['longitude']
+        slapp = Slapp(user_id, time, latitude, longitude)
+        db.session.add(slapp)
+        db.session.commit()
+        return 'Success'
+    else:
+        return 'Error, not enough parameters'
 
-
-@app.route('/api/events', methods=['GET'])
+@app.route('/api/slapps', methods=['GET'])
 def return_all():
-    return jsonify({"events" : events})
+    slapps = Slapp.query.all()
+    return slapps
 
-@app.route('/api/nearby', methods=['GET'])
-def return_nearby():
-    returned_events = []
-    if 'latitude' and 'longitude' and 'time' in request.args:
-        print request.args['latitude'] + " " + request.args['longitude']
-        returned_events = [event for event in events if abs(event['latitude'] - int(request.args['latitude'])) < 1 and abs(event['longitude'] - int(request.args['longitude'])) < 1]
-        print returned_events
-    return jsonify({"events" : returned_events})
+# @app.route('/api/nearby', methods=['GET'])
+# def return_nearby():
+#     returned_events = []
+#     if 'latitude' and 'longitude' and 'time' in request.args:
+#         print request.args['latitude'] + " " + request.args['longitude']
+#         returned_events = [event for event in events if abs(event['latitude'] - int(request.args['latitude'])) < 1 and abs(event['longitude'] - int(request.args['longitude'])) < 1]
+#         print returned_events
+#     return jsonify({"events" : returned_events})
 
 if __name__ == '__main__':
     db.init_app(app)
--- models.py
+++ models.py
@@ -1,20 +1,20 @@
-from flask.ext.sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+from flask.ext.sqlalchemy import SQLAlchemy
 
-Base = declarative_base()
- 
-class Slapp(Base):
+db = SQLAlchemy()
+
+class Slapp(db.Model):
     __tablename__ = 'slapps'
- 
-    id = Column(Integer, primary_key=True)
-    time = Column(DateTime)
-    latitude = Column(Float)
-    longitude = Column(Float)
- 
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, unique=True)
+    time = db.Column(db.DateTime)
+    latitude = db.Column(db.Float)
+    longitude = db.Column(db.Float)
+
     def __init__(self, time, latitude, longitude):
         self.time = time
         self.latitude = latitude
         self.longitude = longitude
- 
+
     def __repr__(self):
         return '<Item %r>' % self.time
